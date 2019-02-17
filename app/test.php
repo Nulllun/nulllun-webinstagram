b2cc322d97497d96e773f24ded339409720d8ba0
Michael Lun <michaeld23046e123@gmail.com>
2019-02-17 16:02:30 +0800

    Test database

--- app/test.php
+++ app/test.php
@@ -4,19 +4,23 @@ echo '<h1>Testing</h1>';
 $pdo = pg_connect(getenv("DATABASE_URL"));
 
 if(!$pdo) {
-    die("Error in connection: " . pg_last_error());
+    die("Error in connection: " . pg_last_error() . '<br>');
 }else{
     echo 'Success connect to database<br>';
 }
+try{
+    pg_exec($pdo,"CREATE TABLE wi_user(uid INT AUTO_INCREMENT PRIMARY KEY, wi_name CHAR(20) NOT NULL, wi_pass CHAR(20) NOT NULL)");
 
-$create_user = pg_query($pdo,"CREATE TABLE wi_user(uid INT AUTO_INCREMENT PRIMARY KEY, wi_name CHAR(20) NOT NULL, wi_pass CHAR(20) NOT NULL)");
+    pg_exec($pdo,"INSERT INTO wi_user(wi_name,wi_pass) VALUES ('admin','minda123')");
 
-$insert_admin = pg_query($pdo,"INSERT INTO wi_user(wi_name,wi_pass) VALUES ('admin','minda123')");
+    pg_exec($pdo,"INSERT INTO wi_user(wi_name,wi_pass) VALUES ('Alice','csci4140')");
 
-$insert_alice = pg_query($pdo,"INSERT INTO wi_user(wi_name,wi_pass) VALUES ('Alice','csci4140')");
+}catch (PDOException $e){
+    echo $e->getMessage().'<br>';
+}
 
-if($create_user) echo 'Success create user table<br>';
-if($insert_admin) echo 'Success insert admin<br>';
-if($insert_alice) echo 'Success insert alice<br>';
+//if($create_user) echo 'Success create user table<br>';
+//if($insert_admin) echo 'Success insert admin<br>';
+//if($insert_alice) echo 'Success insert alice<br>';
 
 ?>
